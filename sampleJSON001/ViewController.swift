18009a097e783e565216f32c94fb3596424655e7
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-03 15:49:11 +0800

    initial commit

--- sampleJSON001/ViewController.swift
+++ sampleJSON001/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        //NSBundleは自分のアプリが動いている場所を返してくる
+        var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+        
+        //pathは読み込むファイルの変数をセットしている・まだ暗号化された文字
+        var jsondata = NSData(contentsOfFile: path!)
+        
+        
+        //ここで変換される
+        let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSArray
+        
+        for dat in jsonArray {
+            
+            //表示
+            print("値=[\(dat)]")
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
